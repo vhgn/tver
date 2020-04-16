1028ab8f8e03490ec98faf9349bf1d3e75f8bbd8
Vahagn <vhgn@pm.me>
2020-04-17 03:06:40 +0400

    Update error logging style

--- src/tver.cpp
+++ src/tver.cpp
@@ -1,28 +1,18 @@
 #include <iostream>
+#include <exception>
 #include "tver.hpp"
 
 int main() {
 	tver::Parser parser;
 
-	try {
-		while (true) {
+	while (true) {
+		try {
+			parser.text = "";
 			std::cout << "> ";
 			std::cin >> parser.text;
 			std::cout << parser.parse() << std::endl;
-		}
-	} catch (tver::Error error) {
-		switch(error) {
-			case tver::Error::UnrecognizedSymbol:
-				std::cout << "Symbol not recognized" << std::endl;
-				break;
-			case tver::Error::ExtraOperand:
-				std::cout << "Extra operand found in expression" << std::endl;
-				break;
-			case tver::Error::UnfinishedExpression:
-				std::cout << "Expression is unfinished or contains an extra operand" << std::endl;
-				break;
-			default:
-				std::cout << "Unexpected error happened" << std::endl;
+		} catch (const std::exception& error) {
+			std::cout << error.what() << std::endl;
 		}
 	}
 }
