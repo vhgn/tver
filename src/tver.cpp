935d33ff960fe4cd2886632139b0971fd7fcb907
Vahagn <vhgn@pm.me>
2020-04-17 17:10:19 +0400

    Fix cin passing first word

--- src/tver.cpp
+++ src/tver.cpp
@@ -9,7 +9,7 @@ int main() {
 		try {
 			parser.text = "";
 			std::cout << "> ";
-			std::cin >> parser.text;
+			std::getline(std::cin, parser.text);
 			std::cout << parser.parse() << std::endl;
 		} catch (const std::exception& error) {
 			std::cout << error.what() << std::endl;
