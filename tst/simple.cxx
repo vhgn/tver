698298b226f6ed1ecbd0ff2fc6880a8dd4830693
Vahagn <vhgn@pm.me>
2020-04-19 02:34:38 +0400

    Add tests for negative numbers

--- tst/simple.cxx
+++ tst/simple.cxx
@@ -4,7 +4,10 @@
 TEST_CASE("Simple expressions") {
 	REQUIRE(tver::Parser("1").parse() == 1);
 	REQUIRE(tver::Parser("1 + 1").parse() == 2);
+	REQUIRE(tver::Parser("-1 + 1").parse() == 0);
 	REQUIRE(tver::Parser("1 + 2 * 3").parse() == 7);
 	REQUIRE(tver::Parser("1 + 2 / 2").parse() == 2);
+	REQUIRE(tver::Parser("-4 + 2 / 2").parse() == -3);
 	REQUIRE(tver::Parser("8 * 9 / 3 - 2 / 4").parse() == 23.5);
+	REQUIRE(tver::Parser("8 * 9 / -3 - 2 / 4").parse() == -24.5);
 }
